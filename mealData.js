6ac67b9e067dd0a7f5af642b43d14ec11908ef48
3x-haust <lyusungyun@icloud.com>
2025-01-02 12:18:16 +0900

    update: mealData.js to return detailed meal information including dish names, meal names, and origin place

--- mealData.js
+++ mealData.js
@@ -6,16 +6,18 @@ export async function getMealData(date1) {
 
   try {
     const response = await axios.get(url);
-    if(response.status != 200)  return ['급식 정보가 없습니다.'];
+    if(response.data.mealServiceDietInfo == undefined)  return {"dishName" : '급식 정보가 없습니다.'};
 
     const mealData = response.data.mealServiceDietInfo[1].row;
     let dishNames = mealData.map(meal => meal.DDISH_NM);
+    let mealName = mealData.map(meal => meal.MMEAL_SC_NM);
+    let orplc = mealData.map(meal => meal.ORPLC_INFO);
 
     dishNames = dishNames.map(dish => {
       return dish.replace(/<[^>]*>/g, '').replace(/\([^)]*\)/g, '').replace(/\*/g, '').replace(/\./g, '');
     });
 
-    return dishNames;
+    return {"dishName" : dishNames, "mealName": mealName, "orplc": orplc};
   } catch (error) {
     console.error(`Error fetching meal data: ${error}`);
   }
